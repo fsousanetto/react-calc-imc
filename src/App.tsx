755d6c86cfef61fbfa9ffee90c75272fe282e8d7
Francisco Netto <franciscon238@gmail.com>
2022-06-16 18:20:40 -0300

    add style grid right

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import styles from './App.module.css';
 import powerImage from './assets/powered.png';
+import { GridItem } from './components/GridItem';
 import { levels, calculateImc } from './helpers/imc';
 
 const App = () => {
@@ -47,7 +48,11 @@ const App = () => {
           <button onClick={handleCalculateButton}>Calcular</button>
         </div>
         <div className={styles.rightSide}>
-          ...
+          <div className={styles.grid}>
+            {levels.map((item,key) => (
+              <GridItem key={key} item={item} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
