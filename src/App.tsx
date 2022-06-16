1cb9c3668aa758b991b7d2ee28a5e9bf8bc003cb
Francisco Netto <franciscon238@gmail.com>
2022-06-16 15:15:59 -0300

    create containers

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import styles from './App.module.css';
+import powerImage from './assets/powered.png';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={powerImage} alt="" width={150} />
+        </div>
       </header>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          ...
+        </div>
+        <div className={styles.rightSide}>
+          ...
+        </div>
+      </div>
     </div>
   );
 }
